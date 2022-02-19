e7f4226ac0d056348036bacaa442b1426b4637a5
hoangm90 <hoangminhquoccool@gmail.com>
2022-02-19 13:10:57 +0100

    add get endpoint

--- _multicontainer/comparer/main.py
+++ _multicontainer/comparer/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 @app.get('/api/makeplan')
 async def compute(request: Request = None):
-    # this is API endpoint, it is expected to run 10 experiments
+    # this is API endpoint, it is expected to run 100 experiments
     input = await request.json()
     return await getResults(10, input)
 
@@ -21,8 +21,10 @@ async def compute(count: int = 10, request: Request = None):
     return await getResults(count, input)
 
 async def getResults(count, input):
+    # from the received input, create the desired input for painter
     needed_input = provide_needed_input.return_needed_input(input)
 
+    # function for requesting painter
     async def fetch():
         async with aiohttp.ClientSession() as session:
             async with session.get(f'http://{serverUrl}:8000/api/makeplan/', json=needed_input) as resp:
@@ -40,16 +42,18 @@ async def getResults(count, input):
     for item in jsons:
         colors = item['colors']
         max_color = item['max_color']
+        chosen_cl = item["chosen_classrooms"]
 
         if best_max_colors == -1:
             best_max_colors = max_color
             best_colors = colors
-            chosen_classrooms = item["chosen_classrooms"]
+            chosen_classrooms = chosen_cl
         elif max_color < best_max_colors:
             best_max_colors = max_color
             best_colors = colors
-            chosen_classrooms = item["chosen_classrooms"]
-    
+            chosen_classrooms = chosen_cl
+
+    # add the best coloring to lectures
     bestResult = add_color_to_lecture.return_planned_timetables(input, best_colors, chosen_classrooms, best_max_colors)
 
     # return the best result
--- _multicontainer/divider/main.py
+++ _multicontainer/divider/main.py
@@ -9,7 +9,31 @@ serverUrl = 'comparer'
 
 app = FastAPI()
 
-@app.websocket('/api/makeplan')
+@app.get('/api/makeplan')
+async def compute(request: Request):
+    
+    # this retrieve JSON structure from request body
+    input = await request.json()
+
+    # now divide the input into parts (two in this case)
+    inputs = divide_data.divide_data(input)
+
+    async def fetch(input):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'http://{serverUrl}:8000/api/makeplan/', json=input) as resp:
+                return await resp.json() 
+
+    # ask services for a solution
+    queries = [fetch(input) for input in inputs]
+    # jsons is list which contains a subsolutions
+    jsons = await asyncio.gather(*queries)
+    
+    # join the subsolutions
+    result = assemble_data.assemble_lessons(jsons[0], jsons[1])
+    # return results
+    return result
+
+@app.websocket('/api/ws')
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
